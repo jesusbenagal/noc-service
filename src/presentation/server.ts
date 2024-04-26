9c13887b81b1c92095ea2b0a11adc15ac21615c1
Jesús <jesusguerreropaniagua6@gmail.com>
2024-04-26 18:53:06 +0200

    feat: started noc project

---
+++ src/presentation/server.ts
@@ -0,0 +1,12 @@
+import { CheckService } from "../domain/use-cases/checks/check-service";
+import { CronService } from "./cron/cron-service";
+
+export class Server {
+  public static start() {
+    console.log("Server started...");
+
+    CronService.createJob("*/5 * * * * *", () => {
+      new CheckService().execute("https://www.google.com");
+    });
+  }
+}
