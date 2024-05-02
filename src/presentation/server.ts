d7a2814e7a14c3297a36b49a93162f73e4951ff8
Jesús <jesusguerreropaniagua6@gmail.com>
2024-05-02 22:38:26 +0200

    feat: postgreSQL implemented

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -2,13 +2,15 @@ import { LogSeverityLevel } from "../domain/entities/log.entity";
 import { CheckService } from "../domain/use-cases/checks/check-service";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { MongoLogDatasource } from "../infrastructure/datasources/mongo-log.datasource";
+import { PostgresLogDatasource } from "../infrastructure/datasources/postgres-log.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
 import { EmailService } from "./email/email.service";
 
 const logRepository = new LogRepositoryImpl(
-  new MongoLogDatasource()
+  // new MongoLogDatasource()
   // new FileSystemDatasource()
+  new PostgresLogDatasource()
 );
 const emailService = new EmailService();
 
