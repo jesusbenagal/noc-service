35c1bf568041b921d894d724c8ebd3bd522e6e52
Jesús Guerrero <jesus.guerrero@talan.com>
2024-05-02 09:25:30 +0200

    feat: configured docker compose for mongo and envs required for connection

--- src/plugins/envs.plugin.ts
+++ src/plugins/envs.plugin.ts
@@ -5,4 +5,10 @@ export const envs = {
   SENDER_MAILER: env.get("SENDER_MAILER").required().asEmailString(),
   SECRET_KEY_MAILER: env.get("SECRET_KEY_MAILER").required().asString(),
   SERVICE_MAILER: env.get("SERVICE_MAILER").required().asString(),
+
+  // Mongo DB
+  MONGO_URL: env.get("MONGO_URL").required().asString(),
+  MONGO_DB_NAME: env.get("MONGO_DB_NAME").required().asString(),
+  MONGO_USER: env.get("MONGO_USER").required().asString(),
+  MONGO_PASS: env.get("MONGO_PASS").required().asString(),
 };
